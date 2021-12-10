17110802f8f65acee5eb6c3b3c1ee6fca81b2489
Grandy1 <ystudyone@gmail.com>
2021-12-10 03:01:36 +0300

    Add logic in Welcome_Activity (verify account)

--- app/src/main/java/com/example/application_towns/Welcome_Activity.java
+++ app/src/main/java/com/example/application_towns/Welcome_Activity.java
@@ -2,13 +2,51 @@ package com.example.application_towns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Welcome_Activity extends AppCompatActivity {
+
+    private String userEmail;
+    private String userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final String[] BaseEmails = getResources().getStringArray(R.array.emails);
+        final String[] BasePasswords = getResources().getStringArray(R.array.passwords);
+        final Intent intent = new Intent(this, NavigationDrawerActivity.class);
+
+        EditText userEmailField = findViewById(R.id.email);
+        EditText userPasswordField = findViewById(R.id.password);
+
+        Button enterButton = findViewById(R.id.enter_button);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userEmail = userEmailField.getText().toString();
+                userPassword = userPasswordField.getText().toString();
+
+                boolean flag = false;
+
+                for (int i = 0; i < 5; i++) {
+                    if (userEmail.equals(BaseEmails[i]) && userPassword.equals(BasePasswords[i]))
+                        flag = true;
+                }
+                if (!flag) {
+                    userEmailField.setTextColor(getResources().getColor(R.color.red));
+                    userPasswordField.setTextColor(getResources().getColor(R.color.red));
+                }
+                else
+                    startActivity(intent);
+            }
+        });
+
     }
 }
